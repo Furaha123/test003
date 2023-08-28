809e447bd00473883944b3febcc4386b299545aa
Furaha123 <furahamoses41@gmail.com>
2023-08-28 21:49:59 +0200

    updated

--- js/app.js
+++ js/app.js
@@ -105,3 +105,19 @@ document
     var form = document.querySelector("form"); // Assuming your form element is a <form> element
     form.reset();
   });
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+navToggle.addEventListener("click", function () {
+  //   console.log(links.classList);
+  //   console.log(links.classList.contains("random"));
+  //   console.log(links.classList.contains("links"));
+
+  //   if (links.classList.contains("show-links")) {
+  //     links.classList.remove("show-links");
+  //   } else {
+  //     links.classList.add("show-links");
+  //   }
+
+  links.classList.toggle("show-links");
+});
